c9a5cbeb261f58744c2d9d65d24dabe1b5db659c
phanicode <63126072+phanicode@users.noreply.github.com>
2020-09-21 03:32:40 +0530

    functional prog ,sys.argv and yaml
    added yaml support , created main
    and interface, addded sys.argv support with comments

--- apps/new.py
+++ apps/new.py
@@ -1,7 +1,8 @@
+# run file as python new.py uv1.csv locations.yaml\
+# python new.py csv_filename yaml_filename
 import sys
 import pandas as pd
 import numpy  as np
-
 import bokeh.layouts        as bl
 import bokeh.models         as bm
 import bokeh.colors         as bc
@@ -13,19 +14,33 @@ import interview.widget as iw
 from eat.io import hops, util
 from bokeh.io import output_file, show
 import matplotlib.pyplot as plt
+import yaml
+import sys
 
-output_file('newtest.html')
-# these are the column values
-csv_fields= [a.strip() for a in """time(UTC),T1,T2,U(lambda),
-V(lambda),Iamp(Jy),Iphase(d),Isigma(Jy)""".split(',')]
 
-def gitcsv_read(file_name):
-    table = pd.read_csv(file_name, names=csv_fields,skiprows=2)
-    return table
-df=gitcsv_read('uv1.csv')
+def main():
+    print(sys.argv)
+    if len(sys.argv)>3:
+        print("you have entered too many arguemnts")
+        return False
+    with open(sys.argv[2], 'r') as f:
+        uvfitscode_color = yaml.load(f)
+    output_file('newtest1.html')
+    # these are the column values
+    csv_fields= [a.strip() for a in """time(UTC),T1,T2,U(lambda),
+    V(lambda),Iamp(Jy),Iphase(d),Isigma(Jy)""".split(',')]
+    df=table = pd.read_csv(sys.argv[1], names=csv_fields,skiprows=2)
+    p = bp.figure(plot_width=400, plot_height=400,output_backend="webgl")
+    
+    for uv_fitscode, color in uvfitscode_color.items():
+        read_all_uv(uv_fitscode, color, p, df)
+    show(p)
+
+
+
 # either do this or manually add all csv files into one in order to replicate 
 # EHT paper
-p = bp.figure(plot_width=400, plot_height=400,output_backend="webgl")
+
 
 # TODO : Convert this into RGB values
 # Eg: bc.HSL(f * i, 0.75, 0.5).to_rgb()
@@ -33,20 +48,23 @@ p = bp.figure(plot_width=400, plot_height=400,output_backend="webgl")
 # https://www.kite.com/python/examples/2926/yaml-dump-a-dictionary-to-a-yaml-document
 
 
-all_location_list=['AA','AP','AZ','JC','LM','PV','SM']
-all_color_list=['red','green','blue','yellow','pink','grey','black']
+# all_location_list=['AA','AP','AZ','JC','LM','PV','SM']
+# all_color_list=['red','green','blue','yellow','pink','grey','black']
 # TODO: Make this a dictionaryy for better readability/sites
-def read_all_uv(location, point_color):
-    a=df.loc[df['T1']==location]
-    print(a.head())
-    b=df.loc[df['T2']==location]
-    a.append(b, ignore_index=True)
-    p.circle(a["U(lambda)"],a["V(lambda)"], size=2, color=point_color)
+def read_all_uv(uv_fitscode, point_color,p,df):
+    """Reads the uv_fitscode
 
-for i in range(len(all_color_list)):
-    read_all_uv(all_location_list[i], all_color_list[i])
+        uv_fitscode: two lettered string like AA, AZ, AP
+        point_color: color of the glyph
+    """
+    first_loc=df.loc[df['T1']==uv_fitscode]
+    second_loc=df.loc[df['T2']==uv_fitscode]
+    first_loc.append(second_loc, ignore_index=True)
+    p.circle(first_loc["U(lambda)"],first_loc["V(lambda)"], size=2, color=point_color)
 
 
 
-show(p)
+
+if __name__ == "__main__":
+    main()
 
